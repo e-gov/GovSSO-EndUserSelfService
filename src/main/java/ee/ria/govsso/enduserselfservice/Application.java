286fbdb1481623f76477a17d33f8d0058212613d
Emma Kuppart <emma.kuppart@nortal.com>
2024-07-08 09:56:44 +0300

    AUT-1548 Remove code that is needed for war packaging

--- src/main/java/ee/ria/govsso/enduserselfservice/Application.java
+++ src/main/java/ee/ria/govsso/enduserselfservice/Application.java
@@ -3,23 +3,14 @@ package ee.ria.govsso.enduserselfservice;
 import co.elastic.apm.attach.ElasticApmAttacher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args) {
         ElasticApmAttacher.attach();
         SpringApplication.run(Application.class, args);
-    }
-
-    // Support servlet container as described in https://docs.spring.io/spring-boot/docs/2.7.x/reference/htmlsingle/#howto.traditional-deployment.war
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        ElasticApmAttacher.attach();
-        return application.sources(Application.class);
     }
 }
